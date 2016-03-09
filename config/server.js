39517c2e44f77e6baa28c1cb12db6d8d7653e9c3
Steven Vandevelde <icid.asset@gmail.com>
2016-03-09 14:24:06 -0800

    Use correct static directory for the express server

--- config/server.js
+++ config/server.js
@@ -1,12 +1,14 @@
+import { resolve } from 'path';
 import chalk from 'chalk';
 import express from 'express';
 
 
 export default function() {
+  const buildDir = resolve(__dirname, '../build');
   const app = express();
   const port = 8080;
 
-  app.use(express.static('../build'));
+  app.use(express.static(buildDir));
 
   app.listen(port, () => {
     console.log(chalk.bold.magenta('Running static server at localhost:8080'));
