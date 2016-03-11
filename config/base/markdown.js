904d42896430c596a8480559424e66d27b90ab55
Steven Vandevelde <icid.asset@gmail.com>
2016-03-11 14:34:12 -0800

    Add code highlighting, etc.

--- config/base/markdown.js
+++ config/base/markdown.js
@@ -6,8 +6,35 @@ import it__mark from 'markdown-it-mark';
 import it__sub from 'markdown-it-sub';
 import it__sup from 'markdown-it-sup';
 
+import Prism from 'prismjs/components/prism-core.js';
 
-const md = it({ html: true });
+import 'prismjs/components/prism-clike.js';
+import 'prismjs/components/prism-markup.js';
+
+import 'prismjs/components/prism-bash.js';
+import 'prismjs/components/prism-c.js';
+import 'prismjs/components/prism-css.js';
+import 'prismjs/components/prism-diff.js';
+import 'prismjs/components/prism-docker.js';
+import 'prismjs/components/prism-elixir.js';
+import 'prismjs/components/prism-erlang.js';
+import 'prismjs/components/prism-git.js';
+import 'prismjs/components/prism-go.js';
+import 'prismjs/components/prism-handlebars.js';
+import 'prismjs/components/prism-haskell.js';
+import 'prismjs/components/prism-http.js';
+import 'prismjs/components/prism-javascript.js';
+import 'prismjs/components/prism-json.js';
+import 'prismjs/components/prism-makefile.js';
+import 'prismjs/components/prism-markdown.js';
+import 'prismjs/components/prism-ruby.js';
+import 'prismjs/components/prism-rust.js';
+import 'prismjs/components/prism-sql.js';
+import 'prismjs/components/prism-swift.js';
+import 'prismjs/components/prism-yaml.js';
+
+
+const md = it({ highlight, html: true });
 
 
 md.use(it__anchor);
@@ -25,4 +52,10 @@ export default function(files, deps) {
       content: md.render(f.content),
     };
   });
+}
+
+
+function highlight(str, lang) {
+  const html = Prism.highlight(str, Prism.languages[lang], lang);
+  return `<pre class="language-${lang}">${html}</pre>`;
 }
